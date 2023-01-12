515f50b4ee49cbb2b2b3da4a038d7c6ac23d8bfb
mny <mny9@outlook.com>
2023-01-12 19:59:41 +0800

    test

--- lib/cyclop.dart
+++ lib/cyclop.dart
@@ -10,6 +10,8 @@ export 'src/widgets/selectors/channels/hsl_sliders.dart';
 export 'src/widgets/selectors/channels/hsl_selector.dart';
 export 'src/widgets/selectors/channels/channel_slider.dart';
 export 'src/widgets/opacity/opacity_slider.dart';
+export 'src/widgets/opacity/opacity_slider_thumb.dart';
+export 'src/widgets/opacity/opacity_slider_track.dart';
 
 export 'src/widgets/eyedrop/eye_dropper_layer.dart';
 export 'src/widgets/eyedrop/eye_dropper_overlay.dart';
